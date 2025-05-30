2c2e7e3875550adff305a069f254899dfc1b8bc6
hinalpithadia <38879377+hinalpithadia@users.noreply.github.com>
2025-05-30 16:55:22 +0530

    pricing page updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -530,3 +530,29 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
+/*==============tooltip==================*/
+document.addEventListener('DOMContentLoaded', () => {
+  const tooltipTriggers = document.querySelectorAll('.tooltip-trigger');
+
+  tooltipTriggers.forEach(trigger => {
+    const tooltip = trigger.nextElementSibling;
+
+    const showTooltip = () => {
+      tooltip.classList.remove('opacity-0', 'translate-y-2', 'invisible');
+      tooltip.classList.add('opacity-100' , 'translate-y-0', 'visible');
+    };
+
+    const hideTooltip = () => {
+      tooltip.classList.remove('opacity-100' , 'translate-y-0', 'visible');
+      tooltip.classList.add('opacity-0', 'translate-y-2', 'invisible');
+    };
+
+    trigger.addEventListener('mouseenter', showTooltip);
+    trigger.addEventListener('mouseleave', hideTooltip);
+    trigger.addEventListener('focus', showTooltip);
+    trigger.addEventListener('blur', hideTooltip);
+
+    // Hide tooltip on click
+    trigger.addEventListener('click', hideTooltip);
+  });
+});
