8363b85f8b5eba7843eb1a9d4bf02ee2cf2761fb
hinalpithadia <38879377+hinalpithadia@users.noreply.github.com>
2025-07-21 12:51:57 +0530

    community pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -637,3 +637,88 @@ document.addEventListener('DOMContentLoaded', () => {
     trigger.addEventListener('click', hideTooltip);
   });
 });
+// ============ SMALL DropDown ================
+
+const selectedAll = document.querySelectorAll(".wrapper-dropdown");
+
+selectedAll.forEach((selected) => {
+  const optionsContainer = selected.children[2];
+  const optionsList = selected.querySelectorAll("div.wrapper-dropdown li");
+
+  selected.addEventListener("click", () => {
+    let arrow = selected.children[1];
+   
+    if (selected.classList.contains("active")) {
+      handleDropdown(selected, arrow, false);
+    } else {
+      let currentActive = document.querySelector(".wrapper-dropdown.active");
+
+      if (currentActive) {
+        let anotherArrow = currentActive.children[1];
+        handleDropdown(currentActive, anotherArrow, false);
+      }
+
+      handleDropdown(selected, arrow, true);
+    }
+  });
+
+  // update the display of the dropdown
+  for (let o of optionsList) {
+    o.addEventListener("click", () => {
+      selected.querySelector(".selected-display").innerHTML = o.innerHTML;
+    });
+  }
+});
+
+// check if anything else ofther than the dropdown is clicked
+window.addEventListener("click", function (e) {
+  if (e.target.closest(".wrapper-dropdown") === null) {
+    closeAllDropdowns();
+  }
+});
+
+// close all the dropdowns
+function closeAllDropdowns() {
+  const selectedAll = document.querySelectorAll(".wrapper-dropdown");
+  selectedAll.forEach((selected) => {
+    const optionsContainer = selected.children[2];
+    let arrow = selected.children[1];
+
+    handleDropdown(selected, arrow, false);
+  });
+}
+
+// open all the dropdowns
+function handleDropdown(dropdown, arrow, open) {
+  if (open) {
+    arrow.classList.add("rotated");
+    dropdown.classList.add("active");
+  } else {
+    arrow.classList.remove("rotated");
+    dropdown.classList.remove("active");
+  }
+}
+/*===card hover video play====*/
+document.querySelectorAll('.card-video').forEach(video => {
+    // ✅ Remove controls on load using JavaScript
+    video.removeAttribute('controls');
+
+    // ✅ Get the card container (assumes parent or grandparent is card)
+    const card = video.closest('.hover-play');
+
+    if (!card) return;
+
+    // Hover behavior
+    card.addEventListener('mouseenter', () => {
+      video.currentTime = 0;
+      video.loop = true;
+      video.play();
+    });
+
+    card.addEventListener('mouseleave', () => {
+      video.loop = false;
+      video.pause();
+      video.currentTime = 0;
+    });
+  });
+
