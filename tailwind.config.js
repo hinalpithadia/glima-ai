5761c751f6de6baab39957677ec7aeb4daa10c8a
hinalpithadia <38879377+hinalpithadia@users.noreply.github.com>
2026-02-18 10:39:08 +0530

    pricing countdown added

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
         'playfair': ['playfair'],
         'bebas': ['Bebas Neue'],
          'manrope': ['manrope'],
+        'agdasima': ['agdasima'],
       },
       colors: {
         'limegreen': '#C6FF00',
